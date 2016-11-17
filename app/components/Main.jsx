e0d84fa7dc09c9253c7e8ea9aecc964ec5afbaaf
Walter Shanahan <Walter Shanahan>
2016-11-17 14:32:51 -0500

    add navigation

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,16 +1,18 @@
 var React = require('react');
 var Nav = require('Nav');
 
-var Main = (props) => {
+var Main = React.createClass({
+  render: function () {
   return (
     <div>
       <Nav/>
       <div className="columns medium-6 large-4 small-centered">
         <p>Main Component</p>
-        {props.children}
+        {this.props.children}
       </div>
     </div>
-  );
-};
+    );
+  }
+});
 
 module.exports = Main;
